e357fe14f1b25d775fef3c18341e6f4d3f0ceaea
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2022-02-22 00:53:47 -0800

    refactor(src): fix bugs; all tests passing

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-export async function importEsm<ImportType = any>(id: string, ...targets: string[]) {
-  const namespace = await import(/* webpack:ignore */ id);
-  return (
-    targets.length <= 1
-      ? targets[0] == '*'
-        ? namespace
-        : namespace.default
-      : targets.reduce(
-          (retval, target) => (retval[target] = namespace[target]),
-          {} as Record<string, unknown>
-        )
-  ) as ImportType;
+export async function importEsm<ImportType = any>(
+  id: string,
+  ...targets: string[]
+): Promise<ImportType> {
+  const namespace = await import(/* webpackIgnore: true */ id);
+
+  // ? Return the default export by default
+  if (targets.length == 0) {
+    return namespace.default;
+  } // ? Otherwise, if a single target is provided, return it
+  else if (targets.length == 1) {
+    return targets[0] == '*' ? namespace : namespace[targets[0]];
+  } // ? Finally, if multiple targets are provided, return them all
+  else {
+    return targets.reduce(
+      (retval, target) => ((retval[target] = namespace[target]), retval),
+      {} as any
+    );
+  }
 }
--- test/unit-index.test.ts
+++ test/unit-index.test.ts
@@ -1,3 +1,66 @@
-// eslint-disable-next-line jest/no-export
-export {};
-test.todo('this');
+import { importEsm } from '../src/index';
+
+jest.mock(
+  'some-lib',
+  () => ({
+    default: () => 'default',
+    aNamedExport: () => 'aNamedExport',
+    bNamedExport: () => 'bNamedExport',
+    cNamedExport: () => 'cNamedExport',
+    __esModule: true
+  }),
+  { virtual: true }
+);
+
+describe('::importEsm', () => {
+  it('imports the default export by default (without typings)', async () => {
+    expect.hasAssertions();
+    const someLib = await importEsm('some-lib');
+    expect(someLib()).toBe('default');
+
+    // someLib() is not a number, but it's has "any" type so should work anyway
+    const someNum: number = someLib();
+    void someNum;
+  });
+
+  it('imports the default export when provided explicitly', async () => {
+    expect.hasAssertions();
+    const someLib = await importEsm('some-lib', 'default');
+    expect(someLib()).toBe('default');
+  });
+
+  it('imports an explicitly provided export (with provided typings)', async () => {
+    expect.hasAssertions();
+
+    const someLib = await importEsm<() => 'aNamedExport'>('some-lib', 'aNamedExport');
+    expect(someLib()).toBe('aNamedExport');
+
+    // @ts-expect-error: someLib() should not be a number
+    const someNum: number = someLib();
+    void someNum;
+  });
+
+  it('imports multiple explicitly provided exports', async () => {
+    expect.hasAssertions();
+
+    const { bNamedExport, cNamedExport } = await importEsm(
+      'some-lib',
+      'bNamedExport',
+      'cNamedExport'
+    );
+
+    expect(bNamedExport()).toBe('bNamedExport');
+    expect(cNamedExport()).toBe('cNamedExport');
+  });
+
+  it('imports special namespace export', async () => {
+    expect.hasAssertions();
+
+    const SomeLib = await importEsm('some-lib', '*');
+
+    expect(SomeLib.default()).toBe('default');
+    expect(SomeLib.aNamedExport()).toBe('aNamedExport');
+    expect(SomeLib.bNamedExport()).toBe('bNamedExport');
+    expect(SomeLib.cNamedExport()).toBe('cNamedExport');
+  });
+});
